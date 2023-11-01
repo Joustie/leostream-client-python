7b0aa40700459903ac1800c056786b4b8d87354f
Joost Evertse <joustie@gmail.com>
2023-11-01 16:28:43 +0100

    Removed code that is now handled in individual classes

--- leostream/__init__.py
+++ leostream/__init__.py
@@ -1,13 +1,8 @@
 # Path: leostream/__init__.py
-from .centers import LeostreamCenters
-from .pools import LeostreamPools
 from .center import LeostreamCenter
 from .pool import LeostreamPool
 from .restapi import LeostreamClient
 from .policy import LeostreamPolicy
-from .policies import LeostreamPolicies
-from .poolassignments import LeostreamPoolAssignments   
 from .poolassignment import LeostreamPoolAssignment
-from .gateways import LeostreamGateways
 from .gateway import LeostreamGateway
 from .webresource import WebResource
