191d9efcab66335299b4a6c675b0578ccada8ae8
Joost Evertse <joustie@gmail.com>
2023-11-01 16:54:25 +0100

    Added more docstrings and refactored order

--- leostream/webresource.py
+++ leostream/webresource.py
@@ -1,20 +1,34 @@
 # pylint: disable=E1101
 import requests
 import json
-import os
 from .restapi import LeostreamClient
 
 class WebResource(object):
     ''' This class is the base class for all Leostream resources found in the API. It contains the common functions
         for all resources. 
-        General design ideas:
-        1. The resource class will have a data attribute that contains the data from the API
-        2. The resource class will have a resource attribute that contains the name of the resource
-        3. The resource class will have a _URL attribute that contains the URL for the resource
-        4. The resource class will have a _HEADERS attribute that contains the headers for the resource
-        5. The resource class will have a _api attribute that contains the LeostreamSession object
-        6. The resource class will have a get() function that will return the data from the API
-        7. The resource class will have a update() function that will update the resource via the API
+
+        # General design ideas for the WebResource class
+
+        There are two types of methods: class methods and instance methods
+
+        ## For the class methods:
+        The base class has a 
+        * list method that will return a list of all resources of the resource_type specified in the url attribute
+        * find method that will find the resource for the name passed to the function.
+
+        The child class will have a   
+        * resource_type attribute that contains the name/type of the resource
+        * some classes can override the base class methods(like list()  or find() ) to add additional functionality -- see poolassignment.py
+
+        ## For the instance methods:
+        The object will have a
+        * data attribute that contains the data from the API
+        * _URL attribute that contains the URL for the resource
+        * _HEADERS attribute that contains the headers for the resource
+        * _api attribute that contains the LeostreamClient singleton object
+        * get() function that will return the data from the API
+        * update() function that will update the resource via the API
+        * create() function that will create the resource via the API (TODO not implemented fully)
 
         Rationale for design:
         - Create classes for Leostream entities like Pools, Centers, Gateways etc.
@@ -23,6 +37,26 @@ class WebResource(object):
         - Create inheritance relation or interface like construction to avoid duplication (login to Leostream/ API get,update)
         '''
     
+    @classmethod
+    def list(cls):
+        '''
+        This method will return a list of all resources of the type specified in the url attribute'''
+
+        cls._api = LeostreamClient()
+        cls._HEADERS = {
+        'Content-Type':'application/json',
+        'Authorization': cls._api._session}
+        cls._URL="https://"+str(cls._api.broker)+"/rest/v1/" + cls.resource_type + "?as+tree=0"
+        
+        response = requests.get(url=cls._URL, headers=cls._HEADERS, verify=False)
+        data = response.json()
+
+        # check https status code
+        if response.status_code != 200:
+            raise Exception("Error: the request returned HTTP status code " + str(response.status_code) + " with the following message: " + str(data))
+
+        return data
+
     @classmethod
     def find(cls, name):
         ''' Goal: Find the resource for the resource name passed to the function.
@@ -74,26 +108,6 @@ class WebResource(object):
 
         return data
     
-    @classmethod
-    def list(cls):
-        '''
-        This method will return a list of all resources of the type specified in the url attribute'''
-
-        cls._api = LeostreamClient()
-        cls._HEADERS = {
-        'Content-Type':'application/json',
-        'Authorization': cls._api._session}
-        cls._URL="https://"+str(cls._api.broker)+"/rest/v1/" + cls.resource_type + "?as+tree=0"
-        
-        response = requests.get(url=cls._URL, headers=cls._HEADERS, verify=False)
-        data = response.json()
-
-        # check https status code
-        if response.status_code != 200:
-            raise Exception("Error: the request returned HTTP status code " + str(response.status_code) + " with the following message: " + str(data))
-
-        return data
-    
     def get(self):
         '''
         This method will return the data for the resource specified in the url attribute'''
@@ -110,40 +124,3 @@ class WebResource(object):
             raise Exception("Error: the request returned HTTP status code " + str(response.status_code) + " with the following message: " + str(data))
 
         return data
-
-    def writefile(self, data):
-        '''
-        This method will write the data from the API to a file in JSON format. It will create a directory for the resource if it does not exist.
-        The file name will be the resource id. It can be used to create a backup of the resource configuration.'''
-        
-        # Save the response to a file
-        jsondir= os.getenv("LEOSTREAM_API_JSONDIR", ".")
-              
-        # If the resource has an id and a pool_id, this is a pool assignment
-        if hasattr(self, '_id') and hasattr(self, '_pool_id') :
-            if not os.path.exists(jsondir + '/'+ self.resource):
-                os.makedirs(jsondir + '/'+ self.resource)  
-
-            if not os.path.exists(jsondir + '/'+ self.resource  + '/' + str(self._pool_id)):
-                os.makedirs(jsondir + '/'+ self.resource + '/' + str(self._pool_id))  
-            
-            filename = jsondir + '/' + self.resource + '/' + str(self._pool_id) + '/' + str(self._id) + '.json'
-
-        # If the resource has an id and no pool_id, this is not a pool assignment
-        elif hasattr(self, '_id') and not hasattr(self, '_pool_id'):
-            if not os.path.exists(jsondir + '/'+ self.resource):
-                os.makedirs(jsondir + '/'+ self.resource) 
-            filename = jsondir + '/' + self.resource + '/' + str(self._id) + '.json'
-
-        # If the resource has a pool_id and no id, this is a pool
-        elif hasattr(self, '_pool_id') and not hasattr(self, '_id'):
-            if not os.path.exists(jsondir + '/'+ self.resource):
-                os.makedirs(jsondir + '/'+ self.resource) 
-            filename = jsondir + '/' + self.resource + '/' + str(self._pool_id) + '.json'
-
-        # If the resource has no id and no pool_id, this is a resource list (centers, gateways, pools, etc)
-        else:    
-            filename = jsondir + '/' + self.resource + '.json'
-
-        with open(filename, 'w') as f:
-            json.dump(data, f, indent=1)
